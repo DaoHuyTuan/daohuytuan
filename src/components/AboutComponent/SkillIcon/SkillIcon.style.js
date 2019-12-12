2360412d09dbccd71a49eb318ca7416aa4362cbd
Tuan Dao <tuan.dao@dinovative.com>
2019-12-13 03:34:29 +0700

    update about page add some icon skill

--- src/components/AboutComponent/SkillIcon/SkillIcon.style.js
+++ src/components/AboutComponent/SkillIcon/SkillIcon.style.js
@@ -1,34 +1,27 @@
 import styled from "styled-components"
 
 export const SkillIconWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  display: flex;
 `
 export const SkillIconStyle = styled.div`
   display: flex;
-  flex-flow: column;
-  width: 200px;
-  height: 200px;
-  box-shadow: 0px 8px 14px 0px #eaeaea;
+  flex-flow: row;
   border-radius: 10px;
   align-items: center;
-  padding: 25px;
+  padding: 10px;
   transition: 0.5s;
   cursor: pointer;
-  &:hover {
-    transition: 0.5s;
-    box-shadow: 0px 8px 14px 0px #cccccc;
-  }
+  margin-right: 20px;
+
   & > img {
     margin-bottom: 0px;
-    width: 100px;
-    height: 100px;
+    width: 50px;
+    height: 50px;
     margin: 0 auto;
-    margin-bottom: 20px;
+    margin-right: 10px;
   }
   & > span {
     font-size: 18px;
     font-family: Montserrat, sans-serif;
-    font-weight: bold;
   }
 `
