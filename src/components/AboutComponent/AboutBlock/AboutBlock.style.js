788576fabfec179260958a515aa37da38c494e0b
Tuan Dao <tuan.dao@dinovative.com>
2019-12-25 03:18:38 +0700

    init v2 change to nextjs

--- src/components/AboutComponent/AboutBlock/AboutBlock.style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const AboutBlockContainer = styled.div`
-  display: flex;
-  flex-flow: column;
-  margin-bottom: 60px;
-  font-family: Montserrat, sans-serif;
-`
--- src/components/AboutComponent/AboutBlockTitle/AboutBlockTitle.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import { fontMonst, colorTheme } from "../../../theme/variable"
-export const AboutBlockTitleStyle = styled.div`
-  font-size: 28px;
-  font-family: ${fontMonst};
-  font-weight: 800;
-  margin-bottom: 28px;
-  color: ${colorTheme};
-`
