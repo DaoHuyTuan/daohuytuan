44f35fe6582344f29be092a02e4751187f8b4926
Tuan Dao <tuandh95@gmail.com>
2020-01-01 23:55:19 +0700

    update

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,39 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
-import { MenuWrapper, MenuItem,MenuList, MenuTitle } from "./Menu.style"
+import { MenuWrapper, MenuItem, MenuList, MenuTitle } from "./Menu.style"
 const Menu = React.memo(({ ...props }) => {
   const { pathname } = props.location || ""
   return (
-    <MenuWrapper >
-      <MenuTitle >
-      <Link to="/"><span>ppdusv</span></Link>  
+    <MenuWrapper>
+      <MenuTitle>
+        <Link to="/">
+          <span>ppdusv</span>
+        </Link>
       </MenuTitle>
       <MenuList>
-      <MenuItem
-        ischecked={pathname.includes("blog") || pathname === "/" ? true : false}
-      >
-        <Link to="/">Blog</Link>
-      </MenuItem>
-      <MenuItem ischecked={pathname === "/about" ? true : false}>
-        <Link to="/about">About</Link>
-      </MenuItem>
-      <MenuItem ischecked={pathname === "/life" ? true : false}>
-        <Link to="/life">Life</Link>
-      </MenuItem>
-      <MenuItem>
-        <a href="./rss.xml" target="_blank">
-          Rss
-        </a>
-      </MenuItem>
-      <MenuItem>
-        <a
-          href="https://app.daohuytuan.com"
-          target="_blank"
-          rel="noopener noreferrer"
+        <MenuItem
+          ischecked={
+            pathname.includes("blog") || pathname === "/" ? true : false
+          }
         >
-          App
-        </a>
-      </MenuItem>
+          <Link to="/">Blog</Link>
+        </MenuItem>
+        <MenuItem ischecked={pathname === "/about" ? true : false}>
+          <Link to="/about">About</Link>
+        </MenuItem>
+        <MenuItem ischecked={pathname === "/life" ? true : false}>
+          <Link to="/life">Life</Link>
+        </MenuItem>
+        <MenuItem>
+          <a href="/rss.xml" target="_blank">
+            Rss
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a
+            href="https://app.daohuytuan.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            App
+          </a>
+        </MenuItem>
       </MenuList>
     </MenuWrapper>
   )
