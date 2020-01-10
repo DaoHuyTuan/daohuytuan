1986579ee0b6ff3c4f9ba7f5a845a39e0d990de4
Tuan Dao <tuan.dao@dinovative.com>
2020-01-10 18:29:56 +0700

    fix bug blog not found in next-config.js

--- next.config.js
+++ next.config.js
@@ -36,6 +36,16 @@ module.exports = withMDX({
             loader: '@svgr/webpack',
           },
         ],
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+            name: "[name].[ext]"
+          }
+        }
       }
     )
 
@@ -48,46 +58,7 @@ module.exports = withMDX({
         return entries
       }
     }
-
     return config
   },
 })
 
-module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const { isServer } = options;
-      nextConfig = Object.assign({ inlineImageLimit: 8192, assetPrefix: "" }, nextConfig);
-
-      if (!options.defaultLoaders) {
-        throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
-      }
-
-      config.module.rules.push({
-        test: /\.(jpe?g|png|svg|gif|ico|webp)$/,
-        exclude: nextConfig.exclude,
-        use: [
-          {
-            loader: require.resolve("url-loader"),
-            options: {
-              limit: nextConfig.inlineImageLimit,
-              fallback: require.resolve("file-loader"),
-              publicPath: `${nextConfig.assetPrefix}/_next/static/images/`,
-              outputPath: `${isServer ? "../" : ""}static/images/`,
-              name: "[name]-[hash].[ext]",
-              esModule: nextConfig.esModule || false
-            }
-          }
-        ]
-      });
-
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
-      }
-
-      return config
-    }
-  })
-}
