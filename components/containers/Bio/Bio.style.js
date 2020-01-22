d69d5a34e8f3874993922f56bb8ee64546398d69
Tuan Dao <tuandh95@gmail.com>
2020-01-23 01:16:17 +0700

    update bio

--- components/containers/Bio/Bio.style.js
+++ components/containers/Bio/Bio.style.js
@@ -2,6 +2,56 @@ import styled from "styled-components";
 
 const BioWrapper = styled.div`
   width: 940px;
+  max-width: 100%;
   margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-flow: column;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
 `;
-export { BioWrapper };
+const Avatar = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+  background-color: #ffffff;
+`;
+const AvatarBorder = styled.div`
+  cursor: pointer;
+  background-image: linear-gradient(
+    to right top,
+    #fcac0f,
+    #fd9522,
+    #fa7f30,
+    #f36a3c,
+    #e85647,
+    #e44751,
+    #dd395b,
+    #d42d65,
+    #d12174,
+    #ca1b85,
+    #be1e96,
+    #ae27a8
+  );
+  width: 208px;
+  height: 208px;
+  border-radius: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+const BioTitle = styled.span`
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 24px;
+`;
+const BioContent = styled.span`
+  font-size: 16px;
+  text-align: center;
+  line-height: 2;
+  word-break: break-word;
+`;
+export { BioWrapper, Avatar, AvatarBorder, BioTitle, BioContent };
--- components/containers/Bio/index.js
+++ components/containers/Bio/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Container from "../Container";
-import { BioWrapper } from "./Bio.style";
+import {
+  BioWrapper,
+  Avatar,
+  AvatarBorder,
+  BioTitle,
+  BioContent
+} from "./Bio.style";
+import avatar from "../../../public/static/images/avatar.svg";
 const Bio = React.memo(props => {
   return (
     <Container clssName="bio">
-      <BioWrapper>FRED</BioWrapper>
+      <BioWrapper>
+        <AvatarBorder>
+          <Avatar src={avatar} />
+        </AvatarBorder>
+        <BioTitle>Hey! I’m David</BioTitle>
+        <BioContent>
+          Breakfast procuring no end happiness allowance assurance frank. Met
+          simplicity nor difficulty unreserved who. Entreaties mr conviction
+          dissimilar me astonished estimating cultivated.
+        </BioContent>
+      </BioWrapper>
     </Container>
   );
 });
